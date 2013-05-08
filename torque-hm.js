68d040e0cb483d3d1bb838fc3b041c9b3ec06d73
Sandro Santilli <strk@keybit.net>
2013-05-08 19:59:18 +0200

    Check for the presence of a table pyramid and use it when available

--- torque-hm.js
+++ torque-hm.js
@@ -4,6 +4,13 @@ L.TileLayer.Torque = L.TileLayer.extend({
     async: true
   },
 
+  z_resolution: function(z) {
+    var earth_circumference = 40075017;
+    var tile_size = 256;
+    var full_resolution = earth_circumference/tile_size;
+    return full_resolution / (Math.pow(2,z));
+  },
+
   initialize: function (options) {
       //This will add 50 particles to the array with random positions
     L.setOptions(this, options);
@@ -30,31 +37,96 @@ L.TileLayer.Torque = L.TileLayer.extend({
         this._master.reset(r)
     }
   },
+  _checkPyramid: function(cb) {
+    // Check it there's any pyramid set for this layer
+    var sql = "SELECT unnest(res) as res from cdb_pyramid.cdb_pyramid " +
+              "WHERE tab = '{0}'::regclass" .format(this.options.table);
+
+    if ( this._pyramid_check_queue ) {
+        this._pyramid_check_queue.push(cb);
+        return;
+    }
+
+    this._pyramid_check_queue = [ cb ];
+
+    console.log("Pyramid checker: " + sql);
+    var that = this;
+
+    var finish = function() {
+        that._pyramid_checked = 1;
+        _.each(that._pyramid_check_queue, function(c) { c(); });
+        delete that._pyramid_check_queue;
+    }
+
+    this._sql.execute(sql)
+       .done(function(data) {
+            console.log("Pyramid checker response has " + data.rows.length + " rows");
+            if ( data.rows.length ) {
+              that._pyramid_resolutions = [];
+              _.each(data.rows, function(r) {
+                that._pyramid_resolutions.push(r.res);
+              });
+            } 
+            finish();
+       })
+       .error(function(err) {
+            console.log(err);
+            finish();
+       });
+
+  },
   _loadTile: function (tile, tilePoint) {
+
+    var that = this;
+
+    if ( ! that._pyramid_checked ) {
+      this._checkPyramid(function() {
+        that._loadTile(tile, tilePoint);
+      });
+      return;
+    }
+
     tile._layer = this;
     this._tracker(1);
-    var that = this;
-    var tileSql = "WITH grid AS (\n" +
-                   " SELECT CDB_RectangleGrid( " +
-                      " CDB_XYZ_Extent({0}, {1}, {2}), ".format(tilePoint.x, tilePoint.y, this._map._zoom) +
-                      " CDB_XYZ_Resolution({0}) * {1}, ".format(this._map._zoom, this.options.resolution) +
-                      " CDB_XYZ_Resolution({0}) * {1} ".format(this._map._zoom, this.options.resolution) +
-                   " ) as cell " +
-                " ),\n" +
-                " hgrid AS (\n" +
-                   " SELECT cell, " +
-                     " round(CAST (st_x(st_centroid(cell)) AS numeric),5) x, " +
-                     " round(CAST (st_y(st_centroid(cell)) AS numeric),5) y " +
-                   " FROM grid " +
-                " )\n" +
-                "SELECT g.x, g.y, {1}({0}) v, '{2}'::text xy "
-                  .format('count(cartodb_id)', this.options.agg, tilePoint.x+":"+tilePoint.y) +
-                "FROM hgrid g, {0} i\n".format(this.options.table) +
+    var tileSql;
+
+    var zoomResolution = this.z_resolution(this._map._zoom);
+    var geoResolution = zoomResolution * this.options.resolution;
+
+    console.log("We need resolution " + geoResolution);
+
+    // TODO: query pyramid if the resolution we need is lower or equal
+    //       than the max pyramid resolution
+    if ( this._pyramid_resolutions && this._pyramid_resolutions[0] <= geoResolution ) {
+      // pick lowest resolution (higher value)
+      var res, i = this._pyramid_resolutions.length;
+      do res = this._pyramid_resolutions[--i]; while ( res > geoResolution );
+      console.log("Using pyramid resolution " + res + " (among " + this._pyramid_resolutions + ")");
+      tileSql = "WITH cte AS ( SELECT ST_SnapToGrid(ST_Centroid(ext), "
+          + "CDB_XYZ_Resolution({0})*{1}) g"
+                  . format(this._map._zoom, this.options.resolution) +
+                ", {1}({0}) v " .format('sum((select sum(v) from cdb_torquepixel_dump(v,0)))', this.options.agg) +
+                "FROM {0}\n".format(this.options.table + '_pyramid') +
+                "WHERE ext && CDB_XYZ_Extent({0}, {1}, {2}) AND res = {3}\n"
+                  .format(tilePoint.x, tilePoint.y, this._map._zoom, res) +
+                " GROUP BY g ) SELECT st_x(g) x, st_y(g) y, v, '{0}'::text xy from cte"
+                  . format(tilePoint.x+":"+tilePoint.y);
+    } else {
+      console.log("Using original table (no pyramid)");
+      tileSql = "WITH cte AS ( SELECT ST_SnapToGrid(i.the_geom_webmercator, "
+          + "CDB_XYZ_Resolution({0})*{1}) g"
+                  . format(this._map._zoom, this.options.resolution) +
+                ", {1}({0}) v " .format('count(cartodb_id)', this.options.agg) +
+                "FROM {0} i\n".format(this.options.table) +
                 "WHERE i.the_geom_webmercator && CDB_XYZ_Extent({0}, {1}, {2})\n"
                   .format(tilePoint.x, tilePoint.y, this._map._zoom) +
-                  " AND ST_Intersects(i.the_geom_webmercator, g.cell)\n" +
-                "GROUP BY g.x, g.y ";
-    
+                " GROUP BY g ) SELECT st_x(g) x, st_y(g) y, v, '{0}'::text xy from cte"
+                  . format(tilePoint.x+":"+tilePoint.y);
+    }
+
+    //console.log(tileSql);
+
+    var that = this;
     this._sql.execute(tileSql)
        .done(function(data) {
             that._redrawTile(data);
